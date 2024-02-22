db60f7dbf1d061200ae7ec4fe10f4ebe26b6668d
Umair2008ali <160146974+Umair2008ali@users.noreply.github.com>
2024-02-22 20:54:43 +0500

    Add files via upload

---
+++ JS class 2/app.js
@@ -0,0 +1,51 @@
+// document.write ( " <table border=2><tr><td> hi </td></table></tr>  <table border=2><tr><td> My </td></table></tr>  <table border=2><tr><td> Name </td></table></tr>  ")   
+
+
+
+// document.write (" <table border=2><tr><td> Is </td></table></tr> <table border=2><tr><td> Umair Ali </td></table></tr>")
+
+
+  
+//  document.write ("<table border=2><tr><td> I </td></table></tr>")
+
+
+
+// document.write( " <table border=2><tr><td> Am </td></table></tr>  <table border=2><tr><td> Doing </td></table></tr>  <table border=2><tr><td> Web & App Development Course </td></table></tr>  ")
+
+alert("Welcome to this page")
+
+
+
+document.write("<b><i><p class="+"para"+">4 + 1 = 4</p></b></i>")
+
+document.write("<b><i><p class="+"para"+">4 + 2 = 8</b></i>")
+document.write("<br>")
+document.write("<br>")
+document.write("<b><i><p class="+"para"+">4 + 3 = 12</b></i>")
+document.write("<br>")
+document.write("<br>")
+document.write("<b><i><p class="+"para"+">4 + 4 = 16</b></i>")
+document.write("<br>")
+document.write("<br>")
+document.write("<b><i><p class="+"para"+">4 + 5 = 20</b></i>")
+document.write("<br>")
+document.write("<br>")
+document.write("<b><i><p class="+"para"+">4 + 6 = 24</b></i>")
+document.write("<br>")
+document.write("<br>")
+document.write("<b><i><p class="+"para"+">4 + 7 = 28</b></i>")
+document.write("<br>")
+document.write("<br>")
+document.write("<b><i><p class="+"para"+">4 + 8 = 32</b></i>")
+document.write("<br>")
+document.write("<br>")
+document.write("<b><i><p class="+"para"+">4 + 9 = 36</b></i>")
+document.write("<br>")
+document.write("<br>")
+document.write("<b><i><p class="+"para"+">4 + 10 = 40</b></i>")
+
+
+
+// var number =5;
+// var cal = --number + ++number -number++ + number + ++number + ++ number + --number - number - ++number - ++number
+// console.log(cal);
